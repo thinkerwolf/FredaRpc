1fca2abd8f7ad257df9434192591551ef1045c58
thinkerwolf <1342125135@qq.com>
2018-09-12 16:42:55 +0800

    Refacting code

---
+++ src/main/java/com/freda/config/ConfigUtil.java
@@ -0,0 +1,34 @@
+package com.freda.config;
+
+import java.util.Collection;
+
+public class ConfigUtil {
+
+	public static RegistryConfig findRegistryConfig(Collection<RegistryConfig> coll, String id) {
+		for (RegistryConfig c : coll) {
+			if (id.equals(c.getId())) {
+				return c;
+			}
+		}
+		return null;
+	}
+
+	public static ClientConfig findClientConfig(Collection<ClientConfig> coll, String id) {
+		for (ClientConfig c : coll) {
+			if (id.equals(c.getId())) {
+				return c;
+			}
+		}
+		return null;
+	}
+
+	public static ServerConfig findServerConfig(Collection<ServerConfig> coll, String id) {
+		for (ServerConfig c : coll) {
+			if (id.equals(c.getId())) {
+				return c;
+			}
+		}
+		return null;
+	}
+
+}
